b69c672b9e7058de12c841e93dee742a9b29490e
Akamigbo <akamigboifeanyi22@gmail.com>
2024-01-05 00:00:03 -0800

    ES6 classes

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,13 @@
+/**
+ * Classroom class
+ */
+export default class ClassRoom {
+  /**
+   * Creates a new ClassRoom class @see {@link ClassRoom}.
+   * @param {Number} maxStudentsSize - The maximimum number
+   * of students
+   */
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
